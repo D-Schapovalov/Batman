3972da829fec91fa7ad7b9579d1edee69e992919
D-Shapovalov <pilot12384@mail.ru>
2022-02-14 18:08:01 +0700

    Added: burger-menu and responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const swiper = new Swiper('.main__slider', {
-	slidesPerView: 2,
 	spaceBetween: 30,
 	loop: true,
 	navigation: {
@@ -8,11 +7,19 @@ const swiper = new Swiper('.main__slider', {
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
-			spaceBetween: 10,
 		},
-		640: {
+		768: {
 			slidesPerView: 2,
-			spaceBetween: 30,
 		},
 	},
+});
+const burgerBtn = document.querySelector('.header__burger');
+const span = document.querySelectorAll('.header__burger-item');
+const menu = document.querySelector('.header__wrap');
+
+burgerBtn.addEventListener('click', () => {
+	span.forEach(element => {
+		element.classList.toggle('header__burger-item_active');
+	});
+	menu.classList.toggle('header__wrap_active');
 });
